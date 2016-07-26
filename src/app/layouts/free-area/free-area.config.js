19d83b1c95865c4c52b47152a90689595a58bc35
vu <vik.uhryn@gmail.com>
2016-07-26 09:35:26 +0300

    updates...

--- src/app/layouts/free-area/free-area.config.js
+++ src/app/layouts/free-area/free-area.config.js
@@ -9,11 +9,20 @@
     function config($stateProvider) {
         $stateProvider.state('free-area', {
             abstract: true,
+            resolve: {
+                authService: 'authService',
+                authResolver: authResolver
+            },
             views: {
                 content: {
                     templateUrl: 'layouts/free-area/free-area.html'
                 }
             }
         });
+    }
+
+    /* ngInject */
+    function authResolver(authService) {
+        return !authService.isAuth();
     }
 })();
--- src/app/layouts/restricted-area/restricted-area.config.js
+++ src/app/layouts/restricted-area/restricted-area.config.js
@@ -9,11 +9,21 @@
     function config($stateProvider) {
         $stateProvider.state('restricted-area', {
             abstract: true,
+            resolve: {
+                authService: 'authService',
+                authResolver: authResolver
+            },
             views: {
                 content: {
                     templateUrl: 'layouts/restricted-area/restricted-area.html'
                 }
             }
         });
+    }
+
+    /* ngInject */
+    function authResolver(authService) {
+        console.log(authService.isAuth());
+        return authService.isAuth();
     }
 })();
